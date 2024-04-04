4e374ff94d921473d25bcc3e15a9b833f670dc0d
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2024-04-04 16:21:06 +0700

    up

--- Quiz/views/bss.py
+++ Quiz/views/bss.py
@@ -3,8 +3,8 @@ Created on Apr 4, 2024
 
 @author: Administrator
 '''
-from _datetime import datetime
 import os
+from pathlib import Path
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -12,6 +12,7 @@ from scipy.io import wavfile
 from scipy.signal import resample
 
 from QuizWebapp.settings import BASE_DIR
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -20,7 +21,11 @@ def source(request):
     context = {}
     if request.method == 'POST':
         file =request.FILES["audio_1"]
-        handle_uploaded_file(file)
+        folder_path,file_name = handle_uploaded_file(file,"audio_1")
+        standard_wav(folder_path,file_name)
+        file =request.FILES["audio_2"]
+        folder_path,file_name = handle_uploaded_file(file,"audio_2")
+        standard_wav(folder_path,file_name)
     return render(request, "bss/source.html", context)
 
 @login_required()
@@ -34,10 +39,9 @@ def source_separation(request):
     context = {}
     return render(request, "bss/source_separation.html", context)
 
-
-def standard_wav(filename, samplerate=44100):
+def standard_wav(folder_path,filename, samplerate=48000):
     # load file
-    rate, data = wavfile.read(filename)
+    rate, data = wavfile.read(os.path.join(folder_path,filename+ '.wav'))
     # convert stereo to mono
     if len(data.shape) > 1:
         data = data[:, 0] / 2 + data[:, 1] / 2
@@ -47,13 +51,19 @@ def standard_wav(filename, samplerate=44100):
     fs = samplerate
     s = data.astype(np.int16)
     folder_path = os.path.join(BASE_DIR, 'Quiz', 'static', 'upload')
-    wavfile.write(folder_path + 'new_', fs, s);
+    new_file_name = 'new_' + filename
+    wavfile.write(os.path.join(folder_path,new_file_name+ '.wav' ), fs, s);
+    img_file_name = 'signal_' + filename
+    img_path = os.path.join(folder_path,img_file_name+ '.png')
+    os.remove(img_path) if os.path.exists(img_path) else None
+    plt.plot(s)
+    plt.savefig(os.path.join(folder_path,img_file_name))
+    plt.close()
     return samplerate, data.astype(np.int16)
-
-def handle_uploaded_file(f):
+def handle_uploaded_file(f,file_name):
     folder_path = os.path.join(BASE_DIR, 'Quiz', 'static', 'upload')
-    strTime = datetime.now().strftime('%d%m%Y_%H%M%S')
-    strFileName = strTime + '_' + f.name
-    with open(os.path.join(folder_path,strFileName), 'wb+') as destination:
+    # strTime = datetime.now().strftime('%d%m%Y_%H%M%S')
+    with open(os.path.join(folder_path,file_name+ '.wav'), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    return folder_path,file_name
