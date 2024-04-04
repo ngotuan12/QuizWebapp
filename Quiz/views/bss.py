1f04193108386604e7e2ba66cabbf1d1c0946b02
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2024-04-04 21:20:31 +0700

    up

--- Quiz/views/bss.py
+++ Quiz/views/bss.py
@@ -8,6 +8,7 @@ import os
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from scipy.io import wavfile
+from scipy.signal import resample
 
 from QuizWebapp.settings import BASE_DIR
 import matplotlib.pyplot as plt
@@ -17,13 +18,13 @@ import numpy as np
 @login_required()
 def source(request):
     context = {}
-    # if request.method == 'POST':
-    #     file =request.FILES["audio_1"]
-    #     folder_path,file_name = handle_uploaded_file(file,"audio_1")
-    #     standard_wav(folder_path,file_name)
-    #     file =request.FILES["audio_2"]
-    #     folder_path,file_name = handle_uploaded_file(file,"audio_2")
-    #     standard_wav(folder_path,file_name)
+    if request.method == 'POST':
+        file =request.FILES["audio_1"]
+        folder_path,file_name = handle_uploaded_file(file,"audio_1")
+        standard_wav(folder_path,file_name)
+        file =request.FILES["audio_2"]
+        folder_path,file_name = handle_uploaded_file(file,"audio_2")
+        standard_wav(folder_path,file_name)
     return render(request, "bss/source.html", context)
 
 @login_required()
@@ -37,7 +38,27 @@ def source_separation(request):
     context = {}
     return render(request, "bss/source_separation.html", context)
 
-
+def standard_wav(folder_path,filename, samplerate=48000):
+    # load file
+    rate, data = wavfile.read(os.path.join(folder_path,filename+ '.wav'))
+    # convert stereo to mono
+    if len(data.shape) > 1:
+        data = data[:, 0] / 2 + data[:, 1] / 2
+    # re-interpolate sample rate    
+    ratio = int(samplerate / rate)
+    data = resample(data, len(data) * ratio)
+    fs = samplerate
+    s = data.astype(np.int16)
+    folder_path = os.path.join(BASE_DIR, 'Quiz', 'static', 'upload')
+    new_file_name = 'new_' + filename
+    wavfile.write(os.path.join(folder_path,new_file_name+ '.wav' ), fs, s);
+    img_file_name = 'signal_' + filename
+    img_path = os.path.join(folder_path,img_file_name+ '.png')
+    os.remove(img_path) if os.path.exists(img_path) else None
+    plt.plot(s)
+    plt.savefig(os.path.join(folder_path,img_file_name))
+    plt.close()
+    return samplerate, data.astype(np.int16)
 def handle_uploaded_file(f,file_name):
     folder_path = os.path.join(BASE_DIR, 'Quiz', 'static', 'upload')
     # strTime = datetime.now().strftime('%d%m%Y_%H%M%S')
